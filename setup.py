b949737eb3fa2a86ba30954fec861f3edf41530b
RajBhattacharya <rajb47483@gmail.com>
2025-07-16 20:52:58 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= "Green_vision",
+    version="0.0.1",
+    author="Raj Bhattacharjee",
+    author_email="rajb47483@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
